36989206d570b1148a1edcd8bffca3fab29e0171
Merter Sualp <merter.sualp@gmail.com>
2021-04-22 06:08:23 +0300

    Solutions and tests for 162

---
+++ 162/SolutionWithHalving.java
@@ -0,0 +1,24 @@
+class SolutionWithHalving {
+    public int findPeakElement(int[] nums) {
+        if (nums.length==1 || nums[0]>nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length-2] < nums[nums.length-1]) {
+            return nums.length-1;
+        }
+        int begin = 1;
+        int end = nums.length-2;
+        while (begin < end) {
+            final int mid = (begin+end) / 2;
+            if (nums[mid] > nums[mid-1]) {
+                if (nums[mid] > nums[mid+1]) {
+                    return mid;
+                }
+                begin = mid+1;
+            } else {
+                end = mid-1;                
+            }
+        }
+        return begin;
+    }
+}
