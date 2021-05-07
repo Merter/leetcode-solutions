5728651910c8a17e5b88c3daddb260185b62a843
Merter Sualp <merter.sualp@gmail.com>
2021-05-07 07:17:18 +0300

    Solution and tests for 33

---
+++ 33/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int begin = 0;
+        int end = nums.length-1;
+        while (begin <= end) {
+            int mid = (begin+end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[begin] <= nums[mid]) {
+                if (nums[begin]<=target && target<=nums[mid]) {
+                    end = mid-1;
+                } else {
+                    begin = mid+1;
+                }
+            } else {
+                if (nums[mid]<=target && target<=nums[end]) {
+                    begin = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
