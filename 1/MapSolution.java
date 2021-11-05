6a7317f87a2a80e73cbd5de3f9b6386aa78a8781
Merter Sualp <merter.sualp@gmail.com>
2021-11-05 14:26:56 +0100

    New tests and modified solution for 1

--- 1/MapSolution.java
+++ 1/MapSolution.java
@@ -1,29 +1,15 @@
-class MapSolution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> num2Indices = new HashMap<>();
+        Map<Integer, Integer> num2Index = new HashMap<>();
         for (int i=0; i<nums.length; i++) {
-            List<Integer> indices = num2Indices.get(nums[i]);
-            if (indices == null) {
-                indices = new ArrayList<>();
-            }
-            indices.add(i);
-            num2Indices.put(nums[i], indices);
-        }
-        for (Map.Entry<Integer, List<Integer>> entry : num2Indices.entrySet()) {
-            int counterPartKey = target-entry.getKey();
-            List<Integer> indices = entry.getValue();
-            List<Integer> counterPartIndices = num2Indices.get(counterPartKey);
-            if (counterPartIndices == null) {
+            int pairToTarget = target - nums[i];
+            Integer pairIndex = num2Index.get(pairToTarget);
+            if (pairIndex == null) {
+                num2Index.put(nums[i], i);
                 continue;
             }
-            if (indices == counterPartIndices) {
-                if (indices.size() == 1) {
-                    continue;
-                }
-                return new int[]{indices.get(0), indices.get(1)};
-            }
-            return new int[]{indices.get(0), counterPartIndices.get(0)};
+            return new int[]{pairIndex, i};
         }
-        return new int[] {-1, -1};
+        return null;    // cannot happen
     }
 }
