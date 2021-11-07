3f5d85d2f83a0878fda9d3e02a96a01a0caeeb51
Merter Sualp <merter.sualp@gmail.com>
2021-11-07 07:28:30 +0100

    A new solution and added tests for 567

--- 567/Solution.java
+++ 567/Solution.java
@@ -1,5 +1,43 @@
 class Solution {
-    public boolean checkInclusion(final String source, final String target) {
+    public boolean checkInclusion(String s1, String s2) {
+        return checkInclusionWithCharCountMatching(s1, s2);
+    }
+
+    private boolean checkInclusionWithCharCountMatching(String s1, String s2) {
+        if (s2.length() < s1.length()) {
+            return false;
+        }
+        int[] canonicalS1 = canonicalForm(s1);
+        int begin = 0;
+        int[] canonicalSubS2 = canonicalForm(s2.substring(begin, s1.length()-1));
+        for (int end=s1.length()-1; end<s2.length(); end++) {
+            canonicalSubS2[s2.charAt(end)-'a']++;
+            if (matches(canonicalS1, canonicalSubS2)) {
+                return true;
+            }
+            canonicalSubS2[s2.charAt(begin++)-'a']--;
+        }
+        return false;
+    }
+    
+    private int[] canonicalForm(String str) {
+        int[] charCounts = new int[26];
+        for (int i=0; i<str.length(); i++) {
+            charCounts[str.charAt(i)-'a']++;
+        }
+        return charCounts;
+    }
+    
+    private boolean matches(int[] charCounts1, int[]charCounts2) {
+        for (int i=0; i<charCounts1.length; i++) {
+            if (charCounts1[i] != charCounts2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkInclusionWithSorting(final String source, final String target) {
         if (target.length() < source.length()) {
             return false;
         }
