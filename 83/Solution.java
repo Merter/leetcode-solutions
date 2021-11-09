e01bc3e2508075e3a16a0d40d09ef36bd6715f62
Merter Sualp <merter.sualp@gmail.com>
2021-11-09 13:09:32 +0100

    Solution and tests for 83

---
+++ 83/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        while (curr.next != null) {
+            if (curr.val != curr.next.val) {
+                curr = curr.next;
+                continue;
+            }
+            ListNode temp = curr.next;
+            curr.next = temp.next;
+            temp.next = null;
+        }
+        return head;
+    }
+}
