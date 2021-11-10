bd17162dec507e092c6005575985b9bb5a1dc49c
Merter Sualp <merter.sualp@gmail.com>
2021-11-10 21:57:37 +0100

    Solution and tests for 53

---
+++ 53/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        for (int i=1; i<nums.length; i++) {            
+            nums[i] = Math.max(nums[i-1]+nums[i], nums[i]);
+            maxSum = Math.max(maxSum, nums[i]);
+        }
+        return maxSum;
+    }
+}
