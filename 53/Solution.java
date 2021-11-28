169af92b0f4efbec73d31085906393e7159abd26
Merter Sualp <merter.sualp@gmail.com>
2021-11-28 18:29:47 +0100

    Better naming of variables for 53

--- 53/Solution.java
+++ 53/Solution.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int maxSum = nums[0];
-        for (int i=1; i<nums.length; i++) {            
-            nums[i] = Math.max(nums[i-1]+nums[i], nums[i]);
-            maxSum = Math.max(maxSum, nums[i]);
+        int sumEndingWithItem = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            sumEndingWithItem = Math.max(nums[i], sumEndingWithItem+nums[i]);
+            maxSum = Math.max(maxSum, sumEndingWithItem);
         }
         return maxSum;
     }
