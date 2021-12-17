5354fcf07a0aef61a01b7760aa68f410725ba715
Merter Sualp <merter.sualp@gmail.com>
2021-12-17 19:19:01 +0100

    Modifications to solution for 938

--- 938/Solution.java
+++ 938/Solution.java
@@ -14,20 +14,16 @@
  * }
  */
 class Solution {
-    public int rangeSumBST(final TreeNode root, final int low, final int high) {
-        int rangeSum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null) {
-            return rangeSum;
-        }        
-        if (root.val>=low && root.val<=high) {
-            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+            return 0;
         }
         if (root.val < low) {
             return rangeSumBST(root.right, low, high);
         }
-        if (root.val > high) {
+        if (high < root.val) {
             return rangeSumBST(root.left, low, high);
         }
-        return rangeSum;
+        return root.val + rangeSumBST(root.left, low, root.val) + rangeSumBST(root.right, root.val, high);
     }
 }
